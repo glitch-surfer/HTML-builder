fc89dccd6b2e43eea95d0502872f7fe6756a3dc5
vladislav-garaev <marker661@gmail.com>
2023-05-06 18:00:56 +0300

    feat task 4 done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const { copyFile, readdir } = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+const folderPath = path.join(__dirname, 'files');
 
+const copyFolder = (folder) => {
+  fs.mkdir(`${folder}-copy`, { recursive: true }, (err) => {
+    if (err) throw err;
+    readdir(`${folder}-copy`).then((data) => {
+      data.forEach(file => {
+        fs.unlink(path.join(`${folder}-copy`, file), (err) => {
+          if(err) throw err;
+        });
+      });
+    });
+  });
+  
+  readdir(folder).then((data) => {
+    data.forEach(file => {
+      const pathToFile = path.join(folder, file);
+
+      fs.stat(pathToFile, (err, stats) => {
+        if (err) throw err;
+        if (stats.isFile()) {
+          copyFile(pathToFile, path.join(`${folder}-copy`, file));
+        } else {
+          copyFolder(pathToFile);
+        }
+      });
+    });
+  });
+};
+copyFolder(folderPath);
