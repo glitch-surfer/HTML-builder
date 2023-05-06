f9d4ec533685dee6f6bd14b5a448d2f934dae78c
vladislav-garaev <marker661@gmail.com>
2023-05-06 11:52:57 +0300

    feat: task 2 done

---
+++ 02-write-file/index.js
@@ -0,0 +1,34 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+const textFile = path.join(__dirname, 'text.txt');
+
+console.log('Здаровеньки');
+console.log('Чего изволишь записать?');
+
+fs.writeFile(
+  textFile,
+  '',
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+stdin.on('data', data => {
+  if (data.toString().trim() === 'exit') process.exit();
+  fs.appendFile(
+    textFile,
+    data,
+    (err) => {
+      if (err) throw err;
+    }
+  );
+});
+
+process.on('exit', () => {
+  console.log('Он улетел, но обещал вернутся!');
+});
+
+process.on('SIGINT', () => {
+  process.exit();
+});
