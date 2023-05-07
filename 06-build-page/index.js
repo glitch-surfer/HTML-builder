0e52874f6fc93a1f66bc73d67f5ca8f0b5f3d020
vladislav-garaev <marker661@gmail.com>
2023-05-07 18:11:49 +0300

    fix: fix regExp and refactor

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,19 +3,19 @@ const fs = require('fs');
 const path = require('path');
 const { open, readdir, copyFile } = require('fs/promises');
 
-const stylesPath = path.join(__dirname, 'styles');
-const bundlePath = path.join(__dirname, 'project-dist');
-const bundleFile = path.join(bundlePath, 'style.css');
-
 //make direction
 const makeDir = async (folder) => {
   await fsProm.rm(folder, { recursive: true, force: true });
   await fsProm.mkdir(folder, { recursive: true });
 };
 
-
 //make css bundle
-const makeCssBundle = async (bundleFolder, assetsFolder) => {
+const stylesPath = path.join(__dirname, 'styles');
+const bundlePath = path.join(__dirname, 'project-dist');
+
+
+const makeCssBundle = async (bundlePath, assetsFolder) => {
+  const bundleFile = path.join(bundlePath, 'style.css');
   await fsProm.writeFile(bundleFile, '');
   const folderData = await readdir(assetsFolder);
 
@@ -28,30 +28,22 @@ const makeCssBundle = async (bundleFolder, assetsFolder) => {
           if (err) throw err;
         });
         stream.on('data', data => {
-          fs.appendFile(
-            bundleFile,
-            data,
-            (err) => {
-              if (err) throw err;
-            }
+          fs.appendFile(bundleFile, data, (err) => { if (err) throw err; }
           );
         });
       }
     });
   }
 };
-// copy assets folder
 
+// copy assets folder
 const assetsPath = path.join(__dirname, 'assets');
 const copiedAssetsPath = path.join(bundlePath, 'assets');
-
-
 
 const copyFolder = async (bundleFolder, assetsFolder) => {
   await makeDir(bundleFolder);
 
   const folderData = await readdir(assetsFolder);
-
   for (const file of folderData) {
     const pathToFile = path.join(assetsFolder, file);
     fs.stat(pathToFile, (err, stats) => {
@@ -64,33 +56,33 @@ const copyFolder = async (bundleFolder, assetsFolder) => {
     });
   }
 };
+
 //maleBundleHtml 
-
-
 const htmlTemplate = path.join(__dirname, 'template.html');
 const componentsFolder = path.join(__dirname, 'components');
 
-
-const changeStr = async (templateStr) => {
+const changeStr = async (templateStr, componentsFolder) => {
   const componentsData = await readdir(componentsFolder);
   let finalStr;
+
   for (const component of componentsData) {
-    const tagName = component.split('.')[0];
-    const componentFilePath = await open(path.join(componentsFolder, component));
-    const componentFile = (await componentFilePath.readFile()).toString();
-    const regexp = new RegExp(`{{${tagName}}}`);
-    finalStr = finalStr ? finalStr.replace(regexp, componentFile) : templateStr.replace(regexp, componentFile);
+    if (path.extname(path.join(componentsFolder, component)) === '.html') {
+      const tagName = component.split('.')[0];
+      const componentFilePath = await open(path.join(componentsFolder, component));
+      const componentFile = (await componentFilePath.readFile()).toString();
+      const regexp = new RegExp(`{{${tagName}}}`, 'ig');
+      finalStr = finalStr ? finalStr.replace(regexp, componentFile) : templateStr.replace(regexp, componentFile);
+    }
   }
   return finalStr;
 };
 
 const makeHtmlBundle = async (template, componentsFolder, bundlePath) => {
   const bundleFile = path.join(bundlePath, 'index.html');
-  
 
   const file = await open(template);
   const templateStr = (await file.readFile()).toString();
-  const finalStr = await changeStr(templateStr);
+  const finalStr = await changeStr(templateStr, componentsFolder);
   await fsProm.writeFile(bundleFile, finalStr);
 };
 
