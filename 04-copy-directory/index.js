ed58eac480407fe5cd71fc846d6dbdf210702d02
vladislav-garaev <marker661@gmail.com>
2023-05-07 20:35:36 +0300

    refactor: change 4 task to better syntax

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,9 +1,10 @@
 const { copyFile, readdir } = require('fs/promises');
+const fsProm = require('fs/promises');
 const fs = require('fs');
 const path = require('path');
 const folderPath = path.join(__dirname, 'files');
-
-const copyFolder = (folder) => {
+//previous solution
+/* const copyFolder = (folder) => {
   fs.mkdir(`${folder}-copy`, { recursive: true }, (err) => {
     if (err) throw err;
     readdir(`${folder}-copy`).then((data) => {
@@ -29,5 +30,30 @@ const copyFolder = (folder) => {
       });
     });
   });
+}; */
+
+const makeDir = async (folder) => {
+  await fsProm.rm(folder, { recursive: true, force: true });
+  await fsProm.mkdir(folder, { recursive: true });
 };
+
+const copyFolder = async (folderPath) => {
+  const newFolder = `${folderPath}-copy`;
+  await makeDir(newFolder);
+
+  const folderData = await readdir(folderPath);
+  for (const file of folderData) {
+    const pathToFile = path.join(folderPath, file);
+    fs.stat(pathToFile, (err, stats) => {
+      if (err) throw err;
+      if (stats.isFile()) {
+        copyFile(pathToFile, path.join(newFolder, file));
+      } //no need recursive function in this task
+      /* else {
+        copyFolder(path.join(newFolder, file), pathToFile);
+      } */
+    });
+  }
+};
+
 copyFolder(folderPath);
